2de6ca9d8de0833b9467f317b95166e3abc4f0e4
tuanngoit <tuanau.it@gmail.com>
2020-10-28 22:39:43 +1100

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
-// import HomePage from './pages/homepage/homepage.component';
-import {Switch, Route, Link} from 'react-router-dom';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from './pages/homepage/homepage.component';
+
 import './App.css';
 
-const HomePage = props => {
-  console.log(props);
-  return (
-    <div>
-      <button onClick={() => props.history.push('/topics')}>Topics</button>
-      <h1>HOME PAGE</h1>
-    </div>
-  );
-};
-
-const TopicsList = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
-    </div>
-  );
-};
-
-const TopicDetail = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-    </div>
-  );
-};
+const HatsPage = props => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className='App'>
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/a/b/topics' component={TopicsList} />
-        <Route path='/a/b/topics/:topicId' component={TopicDetail} />
+        <Route exact path='/a/b/c/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-const HomePage = ({history}) => (
+const HomePage = () => (
 	<div className='homepage'>
 		<h1>Welcome to my Homepage</h1>
 		<Directory />
