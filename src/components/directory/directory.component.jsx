96abb785e2e06ea5a424e2f50a1c945cbfa8f305
tuanngoit <tuanau.it@gmail.com>
2020-10-28 23:50:43 +1100

    Demo {Route, Switch, Link}

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,52 +3,47 @@ import './directory.styles.scss';
 import MenuItem from '../menu-item/menu-item.component';
 
 class Directory extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      sections: [
-        {
-          id: 1,
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          linkUrl: 'hats'
-        }, {
-          id: 2,
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          linkUrl: 'jackets'
-        }, {
-          id: 3,
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          linkUrl: 'sneakers'
-        }, {
-          id: 4,
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          linkUrl: 'womens'
-        }, {
-          id: 5,
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          linkUrl: 'mens'
-        }
-      ]
-    };
-  }
+		this.state = {
+			sections: [
+				{
+					id: 1,
+					title: 'hats',
+					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png'
+				},{
+					id: 2,
+					title: 'jackets',
+					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png'
+				},{
+					id: 3,
+					title: 'sneakers',
+					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png'
+				},{
+					id: 4,
+					title: 'womens',
+					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+					size: 'large'
+				},{
+					id: 5,
+					title: 'mens',
+					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+					size: 'large'
+				}
+			]
+		};
+	}
 
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='directory-menu'>
+				{this.state.sections.map(({id, title, imageUrl, size}) => (
+					<MenuItem key={id} title={title}  imageUrl={imageUrl} size={size}/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default Directory;
