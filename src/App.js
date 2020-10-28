236d622a14fde85cf7fab095333bcc71d7f11952
tuanngoit <tuanau.it@gmail.com>
2020-10-28 19:51:17 +1100

    day4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route, Link} from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+
+const HomePage1 = props => {
+  console.log(props);
+  return (
+    <div>
+      <button onClick={() => props.history.push('/topics')}>Topics</button>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsList = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
+      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
+      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+    </div>
+  );
+};
+
+const TopicDetail = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className='App'>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={HomePage1} />
+        <Route exact path='/a/b/topics' component={TopicsList} />
+        <Route path='/a/b/topics/:topicId' component={TopicDetail} />
+      </Switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-const Homepage = () => (
+const HomePage = () => (
 	<div className='homepage'>
 		<h1>Welcome to my Homepage</h1>
 		<Directory />
 	</div>
 );
 
-export default Homepage;
+export default HomePage;
