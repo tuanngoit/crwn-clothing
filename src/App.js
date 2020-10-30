a3972aa97a3d2849c655c4547739be6310804c02
tuanngoit <tuanau.it@gmail.com>
2020-10-30 13:31:31 +1100

    Getting started with Create React App

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Component } from 'react';
 import './App.css';
 
 
-class App extends Component{
-	render() {
-		return(
-			<div className='App'>
-				Hello World
-			</div>
-		);
-	};
+class App extends Component {
+   render() {
+      return (
+         <div className='App'>
+            Hello World
+         </div>
+      );
+   };
 }
 
 export default App;
